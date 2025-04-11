e0220ccfb383d68e90a28da1de6aebadd34f0d31
mihirdeshmukh1 <mihir.deshmukh@waiin.com>
2025-04-11 09:43:05 +0530

    Modified launch codespaces

--- launch_codespaces.py
+++ launch_codespaces.py
@@ -4,13 +4,11 @@ import os
 
 
 def start_web_server():
-    """Start the Python HTTP server"""
     print("Starting web server on port 8000...")
     subprocess.run(['python', '-m', 'http.server', '8000'])
 
 
 def start_sync_service():
-    """Start the git sync service"""
     print("Starting git sync service...")
     subprocess.run(['python', 'sync_updates.py'])
 
